7e542cbda28d1632984fae79ff69fef61333c98d
Miguel Jhosua Benavides Castillo <mjcbenavides54@gmail.com>
2026-08-31 13:48:45 -0500

    Add safe MST pipeline resume entrypoint

--- run.sh
+++ run.sh
@@ -128,5 +128,10 @@ case ${1:-} in
         else
             start=$(first_unfinished pre); prune_phase pre "$start"; pre_freeze "$start"; publish_freeze; post_freeze
         fi ;;
-    *) echo "Usage: ./run.sh {all|status|resume}" >&2; exit 2 ;;
+    mst-resume)
+        test -z "$(git status --porcelain --untracked-files=no)"
+        sync_code
+        "${ssh_cmd[@]}" "cd \"$REMOTE_ROOT\" && test -f artifacts/test/rois.json && test -f artifacts/selection/top3.json && test -f artifacts/yolov7/final/weights/best.pt && test ! -e artifacts/test/mst && test ! -e artifacts/test/mst_rois.json && test ! -e results/test_mst && ! squeue -u \"\$USER\" -h -o '%j' | grep -q '^v2-'"
+        post_freeze 4 ;;
+    *) echo "Usage: ./run.sh {all|status|resume|mst-resume}" >&2; exit 2 ;;
 esac
